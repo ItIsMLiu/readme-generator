8545e9e6c0d818786ac5e8295714947ee3efeb9d
ItIsMLiu <mliu.design@outlook.com>
2024-01-13 19:21:20 +0000

    array of questions for user completed.

--- index.js
+++ index.js
@@ -5,6 +5,53 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What is the project description?",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What is the table of contents?",
+        name: "contents",
+    },
+    {
+        type: "input",
+        message: "Please enter the installtion details.",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Please enter the usage details?",
+        name: "usage",
+    },
+    {
+        type: "list",
+        message: "Choose a license?",
+        name: "license",
+        choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-clause 'Simplified' license", "BSD 3-clause 'New' or 'Revised' license", "Boost Software License 1.0", "Creative Commons Zero V1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"],
+        default: "MIT License",
+    },
+    {
+        type: "input",
+        message: "Who are the contributors?",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "Please enter tests details.",
+        name: "tests",
+    },
+    {
+        type: "input",
+        message: "Please enter questions details.",
+        name: "questions",
+    },
 
 ];
 
